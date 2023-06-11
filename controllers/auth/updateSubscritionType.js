8f08fd53f53bfb3553cec5d4bfef294c9c38489f
uvchuk <uvchuk@gmail.com>
2023-06-11 20:48:35 +0200

    fix logout&update controllers

--- controllers/auth/updateSubscritionType.js
+++ controllers/auth/updateSubscritionType.js
@@ -1,6 +1,9 @@
+const {User} = require("../../models");
+
 const updateSubscritionType = async (req, res) => {
 	const {subscription} = req.body;
-
+	const {_id} = req.user;
+	await User.findByIdAndUpdate(_id, {subscription});
 	res.json({
 		subscription,
 	});
